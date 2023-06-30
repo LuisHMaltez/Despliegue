1ed18c18e62b97ed9f4b6e419bce0723a7e1f446
LuisHMaltez <101739095+LuisHMaltez@users.noreply.github.com>
2023-06-29 22:25:48 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 443;
+const port = 3000;
 
 app.get('/api/hola', (req, res) => {
   res.send('¡Hola, mundo! by Luis Maltez');
