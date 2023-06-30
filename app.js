6eca784492c8cfc063146c849fcdd786f16f2e75
LuisHMaltez <101739095+LuisHMaltez@users.noreply.github.com>
2023-06-30 08:31:09 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,53 +1,11 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
-app.use(express.json());
-
-const saludos = [{
-        id: 1,
-        text: 'Hola Mundo!',
-        estado: true
-    },
-    {
-        id: 2,
-        text: 'Despliegue de API',
-        estado: true
-    },
-];
-
-app.get('/', (req, res) => {
-    res.send('Saludo Js Api');
+app.get('/api/hola', (req, res) => {
+  res.send('¡Hola, mundo! by Luis Maltez');
 });
 
-app.get('/api/saludos', (req, res) => {
-    res.send(saludos);
-})
-
-app.get('/api/saludos/:id', (req, res) => {
-    const saludo = saludos.find(c => c.id === parseInt(req.params.id));
-    if (!saludo) return res.status(404).send('Saludos no encontrado');
-    else res.send(saludo);
-})
-
-app.post('/api/saludos', (req, res) => {
-    const saludo = {
-        id: saludos.length + 1,
-        text: req.body.text,
-        estado: (req.body.estado === 'true')
-    };
-
-    saludos.push(saludo);
-    res.send(saludo);
+app.listen(port, () => {
+  console.log(`La API está escuchando en http://localhost:${port}`);
 });
-
-app.delete('/api/saludos/:id', (req, res) => {
-    const saludo = saludos.find(c => c.id === parseInt(req.params.id));
-    if (!saludo) return res.status(404).send('Saludos no encontrado');
-
-    const index = saludos.indexOf(saludo);
-    events.splice(index, 1);
-    res.send(saludo);
-});
-
-const port = process.env.port || 443;
-app.listen(port, () => console.log(`Escuchando en puerto ${ port }...`));
